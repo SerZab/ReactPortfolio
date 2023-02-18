75dedcb0564e2794bfb3e98cd748ac69b924a02b
SergeyZ <szabolotnyi777@gmail.com>
2023-02-18 21:56:02 +0100

    New website

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+const Home = () => {
+    return (
+    <>   
+    <Header />
+
+    <main className="section">
+        <div className="container">
+
+                <ul className="content-list">
+                    <li className="content-list__item">
+                        <h2 className="title-2">FrontEnd</h2>
+                        <p>JavaScript, ReactJS, HTML, CSS</p>
+                    </li>
+                    <li className="content-list__item">
+                        <h2 className="title-2">Backend</h2>
+                        <p>Java, Python, Spring, Maven, NodeJS, MySQL, MongoDB</p>
+                    </li>
+                </ul>
+
+        </div>
+    </main>
+    </> 
+    );
+}
+ 
+export default Home;
